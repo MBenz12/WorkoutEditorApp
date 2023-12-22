43ec3535f711a35a1cfcd53437f10b62b1d246a6
mbenz12 <mbenz10120112@gmail.com>
2023-12-22 20:11:46 +0200

    Initial Commit

---
+++ WorkoutEditor/WorkoutEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutEditorApp.swift
+//  WorkoutEditor
+//
+//  Created by Vladyslav on 22.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
